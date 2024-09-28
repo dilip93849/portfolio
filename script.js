113f08dce716fe31085cdb51a2f9dde66c30843a
dilip93849 <142787849+dilip93849@users.noreply.github.com>
2024-09-28 17:14:41 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-   const button = document.getElementById('toggleButton');
-        let isDarkMode = true;
+const button = document.getElementById('toggleButton');
+let isDarkMode = true;
 
-        button.addEventListener('click', () => {
-            document.body.classList.toggle('light-mode');
-            isDarkMode = !isDarkMode;
-            button.textContent = isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode';
-        });
+button.addEventListener('click', () => {
+    document.body.classList.toggle('light-mode');
+    isDarkMode = !isDarkMode;
+    // Update the button's innerHTML to show the correct icon
+    button.innerHTML = isDarkMode ? '<i class="fa-regular fa-sun"></i>' : '<i class="fa-regular fa-moon"></i>';
+});
